65eeb753dcc8418bc4b0c47487fb6d3283feb83e
J. A. Parera Portell <44067227+Parera-Portell@users.noreply.github.com>
2019-09-08 00:48:20 +0200

    Update hk_plot.py

--- hk_plot.py
+++ hk_plot.py
@@ -6,6 +6,9 @@ Joan A. Parera Portell
 
 This script plots H-K stacks.
 
+Usage: python3 hk_plot.py [in file] [out plot] [Hmin] [Hmax] [kmin] [kmax]
+ [bootstrap]
+
 """
 import sys
 import math as m
@@ -23,7 +26,7 @@ if len(sys.argv) < 8:
     print("Usage: python3 hk_plot.py [in file] [out plot] [Hmin] [Hmax] [kmin]\
  [kmax] [bootstrap]")
 
-#----------------------------------Input--------------------------------------#
+#--------------------------------Read input-----------------------------------#
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
@@ -33,6 +36,7 @@ kmin = float(sys.argv[5])
 kmax = float(sys.argv[6])
 boot = sys.argv[7]
 
+# Read files as tables and calculate covariance
 suma = pd.read_csv(infile, sep=",", names=["h", "k", "s"])
 bootstrap = pd.read_csv(boot, sep=",", header=0)
 hstd = float(bootstrap["Hstd"])
@@ -40,26 +44,31 @@ kstd = float(bootstrap["Kstd"])
 corr = float(bootstrap["CORR"])
 covar = corr*hstd*kstd
 
-#--------------------------------Plotting-------------------------------------#
-# Contour data
+#--------------------------Generate contour lines-----------------------------#
 yi = np.linspace(kmin,kmax,150)
 xi = np.linspace(hmin,hmax,150)
 x,y = np.meshgrid(xi,yi)
 zc = sp.griddata((suma["h"],suma["k"]),suma["s"],(x,y), method="linear")
 
-# Error ellipse
-maxs_index = suma.loc[suma["s"]==1].index[0] # find table index of maximum s
-h = float(suma["h"][maxs_index]) # get H and k (center of ellipse)
+#--------------------------Create 1 sigma ellipse-----------------------------#
+# Locate data point of maximmum S
+maxs_index = suma.loc[suma["s"]==suma["s"].max()].index[0]
+# Get H and k (center of ellipse) at data point of maximum S
+h = float(suma["h"][maxs_index]) 
 k = float(suma["k"][maxs_index])
-cov_mat = np.array([[hstd**2, covar],[covar, kstd**2]]) # covariance matrix
+# Calculate covariance matrix
+cov_mat = np.array([[hstd**2, covar],[covar, kstd**2]])
 eigvalues, eigvectors = np.linalg.eig(cov_mat)
+# Obtain axes from eigenvalues
 a = 2*m.sqrt(eigvalues[0]) 
 b = 2*m.sqrt(eigvalues[1])
-tilt = m.degrees(0.5*m.atan(2*covar/((hstd**2)-(kstd**2)))) # counterclockwise
+# Calculation of ellipse tilt
+tilt = m.degrees(0.5*m.atan(2*covar/((hstd**2)-(kstd**2))))
 
-# Plot h-k stack
+#--------------------------------Plotting-------------------------------------#
 plt.figure(figsize=(7,6))
 ax = plt.axes()
+# Grid map
 hkmap = ax.tripcolor(suma["h"],suma["k"],suma["s"], cmap="viridis", zorder=0)
 ax.set_xlim((hmin,hmax))
 ax.set_ylim((kmin,kmax))
@@ -68,10 +77,13 @@ ax.xaxis.set_minor_locator(AutoMinorLocator(5))
 plt.colorbar(hkmap, aspect=25, pad=0.04).set_label("Normalized stack amplitude",
             rotation=270, labelpad=20)
 hkmap.set_clim(0,1)
+# Contour map
 ax.contour(x,y,zc, np.linspace(0.1,0.9,9), colors="k", linewidths=0.1, zorder=1)
 ax.set_ylabel("$\kappa$", fontsize=25)
 ax.set_xlabel("H (km)", fontsize=20)
+# Error ellipse
 ax.add_patch(ep((h,k), a, b, tilt, fill=False, linewidth=1.2, color="r", zorder=2))
+# Central point
 ax.scatter(h,k, 70, color="k", marker="*", zorder=3)
 ax.annotate("H="+str(round(h,1))+" km, $\kappa$="+str(round(k,2)),
             xy=(0.95, 0.92), ha="right", xycoords='axes fraction', fontsize=13, 
